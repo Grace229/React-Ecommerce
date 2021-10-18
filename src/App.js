060da76e1b880a28794c59c87fd59b5529c82ff4
unknown <akpologun.winner@gmail.com>
2021-10-18 13:33:42 +0100

    Header 2 commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import './App.css'
 import Footer from './components/footer/Footer'
+import Header2 from './components/Header/Header2'
 
 const App = () => {
    return (
-      <>
-   
-      </>
+      <div>
+         <Header2 />
+      </div>
    )
 }
 
