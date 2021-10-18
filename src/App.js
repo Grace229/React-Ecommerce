3156f3a5d2e44ac502ab6d118b94625dd5eadf78
Grace229 <itiunggrace@gmail.com>
2021-10-18 18:19:56 +0100

    trending

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Testimonial from './testimonial/Testimonial'
+import Trending from './Trending/Trending'
+
+const App = () => {
+    return (
+        <div>
+            <Trending />
+            <Testimonial />
+        </div>
+    )
+}
+
+export default App
