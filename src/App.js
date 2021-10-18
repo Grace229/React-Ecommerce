01eba7f4c0656a13e5fe8d824fd917ba806537e8
unknown <akpologun.winner@gmail.com>
2021-10-18 18:17:41 +0100

    Hero section commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import './App.css'
-import Footer from './components/footer/Footer'
+// import Footer from './components/footer/Footer'
 import Header1 from './components/Header/Header1'
 import Header2 from './components/Header/Header2'
+import Hero from './components/Hero/Hero'
 
 const App = () => {
    return (
       <div>
          <Header1 />
          <Header2 />
+         <Hero />
       </div>
    )
 }
