96b9e222727ff1c10d4afee157965af010f62c6b
GodfreySam <godfreysam09@gmail.com>
2021-10-22 09:21:39 +0100

    updated footer font

--- src/App.js
+++ src/App.js
@@ -3,29 +3,29 @@ import "./App.css";
 import Footer from "./components/footer/Footer";
 import PageSeven from "./components/pageSeven/PageSeven";
 import Pagesix from "./components/pageSix/Pagesix";
-import Header1 from "./components/Header/Header1";
-import Header2 from "./components/Header/Header2";
-import Hero from "./components/Hero/Hero";
-import Benefits from './components/Benefit/Benefits'
-import Arrivals from "./components/Arrival/Arrivals";
-import Testimonial from "./testimonial/Testimonial";
+// import Header1 from "./components/Header/Header1";
+// import Header2 from "./components/Header/Header2";
+// import Hero from "./components/Hero/Hero";
+// import Benefits from './components/Benefit/Benefits'
+// import Arrivals from "./components/Arrival/Arrivals";
+// import Testimonial from "./testimonial/Testimonial";
 // import Trending from "./Trending/Trending";
-import Product from './components/Product/Product';
+// import Product from './components/Product/Product';
 
 const App = () => {
 	return (
 		<>
-			<Header1 />
+			{/* <Header1 />
 			<Header2 />
-			<Hero />
-			<Benefits />
+			<Hero /> */}
+			{/* <Benefits />
 			<Arrivals />
-			{/* <Trending /> */}
-			<Testimonial />
+			<Trending />
+			<Testimonial /> */}
 			<Pagesix />
 			<PageSeven />
 			<Footer />
-			<Product />
+			{/* <Product /> */}
 		</>
 	);
 };
