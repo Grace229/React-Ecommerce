520c5cf1c3ba5ca090b91bdbef52e72e989d74ea
GodfreySam <godfreysam09@gmail.com>
2021-10-22 20:17:36 +0100

    reverting main to freyyo

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./App.css";
+import Footer from "./components/footer/Footer";
+import PageSeven from "./components/pageSeven/PageSeven";
+// import Pagesix from "./components/pageSix/Pagesix";
+import Header1 from "./components/Header/Header1";
+import Header2 from "./components/Header/Header2";
+// import Hero from "./components/Hero/Hero";
+// import Testimonial from "./testimonial/Testimonial";
+// import Trending from "./Trending/Trending";
+// import Product from './components/Product/Product';
+// import ShoppingCart from './components/Shopping/ShoppingCart';
+
+const App = () => {
+	return (
+		<>
+			<Header1 />
+			<Header2 />
+			{/* <Hero />
+			<Trending />
+			<Testimonial />
+			<Pagesix />*/}
+			<PageSeven />
+
+			{/* <Product />  */}
+			{/* <ShoppingCart /> */}
+			<Footer />
+		</>
+	);
+};
+
+export default App;
