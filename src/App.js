16e8b00ae77f23cfa1c6961d668edcbf2eb2c96c
GodfreySam <godfreysam09@gmail.com>
2021-10-23 23:46:29 +0100

    updated main with MDWeena branch

--- src/App.js
+++ src/App.js
@@ -6,25 +6,30 @@ import PageSeven from "./components/pageSeven/PageSeven";
 import Pagesix from "./components/pageSix/Pagesix";
 import Header1 from "./components/Header/Header1";
 import Header2 from "./components/Header/Header2";
-// import Hero from "./components/Hero/Hero";
-// import Testimonial from "./testimonial/Testimonial";
-// import Trending from "./Trending/Trending";
-// import Product from './components/Product/Product';
-// import ShoppingCart from './components/Shopping/ShoppingCart';
+import Hero from "./components/Hero/Hero";
+import Benefits from "./components/Benefit/Benefits.jsx";
+import Arrivals from "./components/Arrival/Arrivals.jsx";
+import Categories from "./components/Category/Categories.jsx";
+import Testimonial from "./testimonial/Testimonial";
+import Trending from "./Trending/Trending";
+import Product from './components/Product/Product';
+import ShoppingCart from './components/Shopping/ShoppingCart';
 
 const App = () => {
 	return (
 		<>
-			{/* <Header1 />
-			<Header2 /> */}
-			{/* <Hero /> */}
-			{/* <Trending />
-			<Testimonial /> */}
+			<Header1 />
+			<Header2 />
+			<Hero />
+			<Benefits />
+			<Arrivals />
+			<Categories />
+			<Trending />
+			<Testimonial />
 			<Pagesix />
 			<PageSeven />
-
-			{/* <Product />  */}
-			{/* <ShoppingCart /> */}
+			<Product /> 
+			 <ShoppingCart />
 			<Footer />
 			<MainFooter />
 		</>
